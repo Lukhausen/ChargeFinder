a804bd09f3d4b57f685d1b5f500c51d6885e1f4d
lmarschh <lmarschh@cisco.com>
2023-11-06 17:33:10 +0100

    init

---
+++ app/src/main/java/com/example/chargefinder/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.chargefinder
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.chargefinder.ui.theme.ChargeFinderTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val buttonStartMapActivity: Button = findViewById(R.id.button_map)
+
+        buttonStartMapActivity.setOnClickListener{
+            val intent = Intent(this, MapActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/chargefinder/MapActivity.kt
@@ -0,0 +1,15 @@
+package com.example.chargefinder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MapActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_map)
+
+
+
+    }
+}
